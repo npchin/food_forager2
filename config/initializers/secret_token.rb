a9812385cebfdba3f81b17c160de6299d955972a
Nicholas Chin <nchin2014@kellogg.northwestern.edu>
2013-11-21 00:25:19 -0600

    New app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodForager2::Application.config.secret_key_base = 'f2435a3ed8bb166f5a588ecd3dbfdcf221a2c4896b80ac16f33ae1cd131e5755695cec2b0ca1da92920034ba10abc3a692be1399efe898b56126d0841a8a19ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FoodForager2::Application.config.session_store :cookie_store, key: '_food_forager2_session'
