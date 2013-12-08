2bd63f225328a5347fc63a9a3a5d7100b00a8085
Nicholas Chin <nchin2014@kellogg.northwestern.edu>
2013-12-08 15:57:10 -0600

    Added event location validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,15 +1,14 @@
 class Event < ActiveRecord::Base
 	has_many :food_details
 
+	validates :location, :presence => true
+
 	mount_uploader :event_photo, EventPhotoUploader
 
 	def full_event
 		return cuisine + " " + location + last_updated.to_s
 	end
 
-	#def full_event_with_created_date
-	#	return cuisine + " " + location + created_at.to_s
-	#end
 
 	def last_updated
 		if food_details.length > 0
