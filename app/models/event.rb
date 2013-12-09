d55026c8d9d0be4b3a016a9e0d56d71f525bb8af
Nicholas Chin <nchin2014@kellogg.northwestern.edu>
2013-12-08 18:02:55 -0600

    Updated event last updated logic

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,16 +9,21 @@ class Event < ActiveRecord::Base
 	mount_uploader :event_photo, EventPhotoUploader
 
 	def full_event
-		return cuisine + " " + location + last_updated.to_s
+		return cuisine + " " + location
 	end
 
 
 	def last_updated
-		if food_details.length > 0
-		return food_details.order("updated_at DESC").first.updated_at
-	else
-		return  updated_at
-	end
+		if food_details.any?
+			if food_details.order("updated_at DESC").first.updated_at > updated_at
+				return food_details.order("updated_at DESC").first.updated_at
+			else
+				return updated_at
+			end
+			
+		else
+			return  updated_at
+		end
 
 		# Go get all the food_details that belong to this event, ordered by updated_at descending
 		# Return the updated_at for the first row in the collection
